9be2591ef8e6ad5cf3905fc3ad61fec08dda0519
xxm <xxm13504577723@163.com>
2019-10-21 20:30:30 +0800

    add pagination;complite news

--- CarAccessories/CarNews/views.py
+++ CarAccessories/CarNews/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from .models import *
 from .serializers import NewsSerializer
 from rest_framework.response import Response
